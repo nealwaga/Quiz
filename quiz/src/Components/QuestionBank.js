e1061c290c96ffdb5755da046f56b2fbabc821b2
nealwaga <waganeal@gmail.com>
2023-07-31 12:34:29 +0300

    created questions

---
+++ quiz/src/Components/QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the maximum possible length of an identifier?",
+        options: ["16", "32", "64", "None of the above"],
+        answer: "None of the above",
+    },
+    {
+        id: 2,
+        question: "Which of the following cannot be a variable?",
+        options: ["_val", "val", "try", "_try_"],
+        answer: "try",   
+    },
+    {
+        id: 3,
+        question: "Which of the following is correctly evaluated for this function?",
+        options: ["(x**y) / z", "(x / y) * z", "(x**y) % z", "(x / y) / z"],
+        answer: "(x**y) % z",
+    },
+    {
+        id: 4,
+        question: "Which of the following types of loops are not supported in Python?",
+        options: ["for", "while", "do-while", "None of the above"],
+        answer: "do-while",
+    },
+    {
+        id: 5,
+        question: "Which of the following is the proper syntax to check if a particular element is present in a list?",
+        options: ["if ele in list", "if not ele not in list", "Both A and B", "None of the above"],
+        answer: "BOth A and B",
+    },
+]
+
+export default qBank;
